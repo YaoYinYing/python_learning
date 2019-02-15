fe6d4d2e801a8214e9db5eaf0191e5dbe721c798
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-02-15 21:02:56 +0800

    Create const.py

---
+++ Ebook_downloader/book118.com/const.py
@@ -0,0 +1,5 @@
+headers = {'Host': 'max.book118.com', 'Referer': 'https://max.book118.com/index.php?m=Download&id=%s',
+           'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
+                         'AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/63.0.3239.132 '
+                         'Mobile Safari/537.36'}
