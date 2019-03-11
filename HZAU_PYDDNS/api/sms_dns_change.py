1a5957cdb200e76697953f245ce6eda5a6f96dc5
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-03-11 13:31:42 +0800

    Create sms_dns_change.py

---
+++ HZAU_PYDDNS/api/sms_dns_change.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import sys
+from aliyunsdkdysmsapi.request.v20170525 import SendSmsRequest
+from aliyunsdkdysmsapi.request.v20170525 import QuerySendDetailsRequest
+from aliyunsdkcore.client import AcsClient
+import uuid
+from aliyunsdkcore.profile import region_provider
+from aliyunsdkcore.http import method_type as MT
+from aliyunsdkcore.http import format_type as FT
+import const
+import time
+import json
+import traceback
+
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+except NameError:
+    pass
+except Exception as err:
+    raise err
+
+# 注意：不要更改
+REGION = "cn-hangzhou"
+PRODUCT_NAME = "Dysmsapi"
+DOMAIN = "dysmsapi.aliyuncs.com"
+
+acs_client = AcsClient(const.ACCESS_KEY_ID, const.ACCESS_KEY_SECRET, REGION)
+region_provider.add_endpoint(PRODUCT_NAME, REGION, DOMAIN)
+
+def send_sms(business_id, phone_numbers, sign_name, template_code, template_param=None):
+    smsRequest = SendSmsRequest.SendSmsRequest()
+    smsRequest.set_TemplateCode(template_code)
+    if template_param is not None:
+        smsRequest.set_TemplateParam(template_param)
+
+    smsRequest.set_OutId(business_id)
+    smsRequest.set_SignName(sign_name)
+    smsRequest.set_PhoneNumbers(phone_numbers)
+
+    # 调用短信发送接口，返回json
+    smsResponse = acs_client.do_action_with_exception(smsRequest)
+
+    return smsResponse
+
+def dns_record_update_sms(name, newip):
+    __business_id = uuid.uuid1()
+    params = "{\"pcname\":\"" + name +"\",\"newip\":\"" + newip + "\"}"
+    try:
+        send_sms(__business_id, const.admin_phonenumber, const.sms_signature, const.sms_temp_code, params)
+    except Exception as e:
+        print({"500": "Fail to notify the administrator."})
+
+
+   
+    
+    
+
