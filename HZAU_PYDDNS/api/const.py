f63ac13db0fac832cbc13673388c025280c85765
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-03-11 13:33:45 +0800

    Create const.py

---
+++ HZAU_PYDDNS/api/const.py
@@ -0,0 +1,12 @@
+# def the client inf.
+from aliyunsdkcore.client import AcsClient
+client = AcsClient("Client Data Here")
+ACCESS_KEY_ID = "Access Key Here"
+ACCESS_KEY_SECRET = "Secret Here"
+
+token ="token here"
+domain_name ="example.com"
+
+admin_phonenumber="13512341234"
+sms_signature="Signature"
+sms_temp_code="temp_code"
