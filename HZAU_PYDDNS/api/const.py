b69d01b13be9feefcb407a95a56b29402a3f1190
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-09-24 12:32:11 +0800

    add serverchan setting

--- HZAU_PYDDNS/api/const.py
+++ HZAU_PYDDNS/api/const.py
@@ -1,12 +1,19 @@
 # def the client inf.
 from aliyunsdkcore.client import AcsClient
-client = AcsClient("Client Data Here")
-ACCESS_KEY_ID = "Access Key Here"
-ACCESS_KEY_SECRET = "Secret Here"
 
-token ="token here"
-domain_name ="example.com"
+# aliyun cloud service setting
+client = AcsClient()
+ACCESS_KEY_ID = ""
+ACCESS_KEY_SECRET = ""
 
-admin_phonenumber="13512341234"
-sms_signature="Signature"
-sms_temp_code="temp_code"
+# Server setting
+token =""
+domain_name =""
+
+# SMS message setting
+admin_phonenumber=""
+sms_signature=""
+sms_temp_code=""
+
+# serverchan message setting
+serverchan_SCKEY=""
