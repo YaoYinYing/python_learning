56af26645d418f8384ceafc379a10534cfa76b99
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-09-30 18:39:59 +0800

    add domain name list support, request method limit

--- HZAU_PYDDNS/api/const.py
+++ HZAU_PYDDNS/api/const.py
@@ -1,6 +1,10 @@
 # def the client inf.
 from aliyunsdkcore.client import AcsClient
 
+
+# request method limit, set True if post is allowed only.
+request_method_limit=False
+
 # aliyun cloud service setting
 client = AcsClient()
 ACCESS_KEY_ID = ""
@@ -8,12 +12,15 @@ ACCESS_KEY_SECRET = ""
 
 # Server setting
 token =""
+# default domain
 domain_name =""
+# domain list
+domain_name_list =['']
 
-# SMS message setting
+# SMS message setting, remain blank if not used.
 admin_phonenumber=""
 sms_signature=""
 sms_temp_code=""
 
-# serverchan message setting
+# serverchan message setting, remain blank if not used.
 serverchan_SCKEY=""
