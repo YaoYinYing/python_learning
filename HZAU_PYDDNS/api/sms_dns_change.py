adafabc8e308b1f394223eca58e72154f13fb345
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-09-30 18:43:37 +0800

    alter sms sending by defining phone number

--- HZAU_PYDDNS/api/sms_dns_change.py
+++ HZAU_PYDDNS/api/sms_dns_change.py
@@ -3,23 +3,24 @@
 
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.request import CommonRequest
+import requests
 import const
 client = AcsClient(const.ACCESS_KEY_ID, const.ACCESS_KEY_SECRET, 'cn-hangzhou')
 def dns_record_update_sms(name, newip):
-    request = CommonRequest()
-    request.set_accept_format('json')
-    request.set_domain('dysmsapi.aliyuncs.com')
-    request.set_method('POST')
-    request.set_protocol_type('https') # https | http
-    request.set_version('2017-05-25')
-    request.set_action_name('SendSms')
-
-    request.add_query_param('RegionId', "cn-hangzhou")
-    request.add_query_param('PhoneNumbers', const.admin_phonenumber)
-    request.add_query_param('SignName', const.sms_signature)
-    request.add_query_param('TemplateCode', const.sms_temp_code)
-    request.add_query_param('TemplateParam', "{\"pcname\":\"" + name +"\",\"newip\":\"" + newip + "\"}")
-
-    response = client.do_action(request)
-    # python2:  print(response) 
-    print(str(response, encoding = 'utf-8'))
+    if const.admin_phonenumber !='':
+        request = CommonRequest()
+        request.set_accept_format('json')
+        request.set_domain('dysmsapi.aliyuncs.com')
+        request.set_method('POST')
+        request.set_protocol_type('https') # https | http
+        request.set_version('2017-05-25')
+        request.set_action_name('SendSms')
+        request.add_query_param('RegionId', "cn-hangzhou")
+        request.add_query_param('PhoneNumbers', const.admin_phonenumber)
+        request.add_query_param('SignName', const.sms_signature)
+        request.add_query_param('TemplateCode', const.sms_temp_code)
+        request.add_query_param('TemplateParam', "{\"pcname\":\"" + name +"\",\"newip\":\"" + newip + "\"}")
+        response = client.do_action(request)
+        # print(str(response, encoding = 'utf-8'))
+    else:
+        pass
