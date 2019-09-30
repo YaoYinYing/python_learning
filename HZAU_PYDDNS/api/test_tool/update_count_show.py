f07c404959d935c194715d56e94ad566c40f868c
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-09-30 19:35:08 +0800

    Update update_count_show.py

--- HZAU_PYDDNS/api/test_tool/update_count_show.py
+++ HZAU_PYDDNS/api/test_tool/update_count_show.py
@@ -4,12 +4,18 @@ import time,datetime
 import traceback
 import cgi
 
+hide_column = 2
 
 def ddns_count_show():
     conn = sqlite3.connect("sqlite file")
     c = conn.cursor()
     sql = '''SELECT * FROM DDNS_UPDATE_COUNT;''' 
-    
+    print("<html><table border='5' cellspacing='2'> ")
+    print("<thead><tr>")
+    for x in ["record_id","domain name", "name", "count", "latest check"][hide_column:]:
+        print("<th>%s</th>" % x )
+    print("</tr></thead>")
+    print("<tbody>")
     try:
         data=c.execute(sql)
         for item in data:
@@ -17,9 +23,11 @@ def ddns_count_show():
             date_now = time.mktime(time.strptime(time.strftime(str(datetime.datetime.now()).split(" ")[0]),"%Y-%m-%d")) 
 
             if int(date_now)-int(date_latest_check) < 86400:
-                print(item)
-                print("</br>")
-
+                print("<tr>")
+                for x in item[hide_column:]:
+                    print("<td>%s</td>" %x )
+                print("</tr>")
+        
 
     except Exception as e:
         traceback.print_exc()
@@ -27,7 +35,7 @@ def ddns_count_show():
         conn.close()
 
     pass
-
+    print("</tbody></table></html>")
 print("content-type:text/html")
 print("")
 print("<meta http-equiv = 'refresh' content = 30 />")
