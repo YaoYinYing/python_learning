47294d3784af14dbefa8abb267badcb843bc128f
Add a name <33014714+YaoYinYing@users.noreply.github.com>
2019-10-28 11:52:26 +0800

    解决pcname过长无法发送，过短重复处理

--- HZAU_PYDDNS/api/sms_dns_change.py
+++ HZAU_PYDDNS/api/sms_dns_change.py
@@ -8,6 +8,8 @@ import const
 client = AcsClient(const.ACCESS_KEY_ID, const.ACCESS_KEY_SECRET, 'cn-hangzhou')
 def dns_record_update_sms(name, newip):
     if const.admin_phonenumber !='':
+        if len(name) > 20:
+            name =  name[:15] + "--" + name[-3:]
         request = CommonRequest()
         request.set_accept_format('json')
         request.set_domain('dysmsapi.aliyuncs.com')
@@ -19,7 +21,7 @@ def dns_record_update_sms(name, newip):
         request.add_query_param('PhoneNumbers', const.admin_phonenumber)
         request.add_query_param('SignName', const.sms_signature)
         request.add_query_param('TemplateCode', const.sms_temp_code)
-        request.add_query_param('TemplateParam', "{\"pcname\":\"" + name[:15] + "--" + name[-3:] +"\",\"newip\":\"" + newip[:20] + "\"}")
+        request.add_query_param('TemplateParam', "{\"pcname\":\"" + name +"\",\"newip\":\"" + newip + "\"}")
         response = client.do_action(request)
         # print(str(response, encoding = 'utf-8'))
     else:
